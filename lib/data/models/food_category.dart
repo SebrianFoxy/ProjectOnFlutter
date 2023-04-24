d2858f5dd25185062e0f4737951e724650738ec7
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-04-24 21:33:01 +0300

    данные

---
+++ lib/data/models/food_category.dart
@@ -0,0 +1,9 @@
+import 'package:JapanEatProject/data/models/food.dart';
+//import '_models.dart';
+
+class FoodCategory {
+  final FoodType type;
+  bool isSelected;
+
+  FoodCategory(this.type, this.isSelected);
+}
---
+++ lib/states/food_state.dart
@@ -0,0 +1,5 @@
+class FoodState {
+  FoodState._();
+  static final _instance = FoodState._();
+  factory FoodState() => _instance;
+}
