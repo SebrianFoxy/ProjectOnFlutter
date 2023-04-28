956da81dad80fde7696768a2669b7e1d0f57135b
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-04-28 23:31:53 +0300

    логика клика

--- lib/ui/screens/food_list_screen.dart
+++ lib/ui/screens/food_list_screen.dart
@@ -2,6 +2,8 @@ import 'package:badges/badges.dart';
 import 'package:flutter/material.dart' hide Badge;
 import 'package:JapanEatProject/ui_kit/_ui_kit.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../../ui_kit/_ui_kit.dart';
+import '../../data/app_data.dart';
 
 class FoodList extends StatefulWidget {
   const FoodList({super.key});
@@ -11,6 +13,8 @@ class FoodList extends StatefulWidget {
 }
 
 class FoodListState extends State<FoodList> {
+  var categories = AppData.categories;
+
   @override
   Widget build(BuildContext context) => Scaffold(
       appBar: _appBar(context),
@@ -21,7 +25,7 @@ class FoodListState extends State<FoodList> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                "Morning, Mavi",
+                "Morning, Bohdan",
                 style: Theme.of(context).textTheme.headlineSmall,
               ),
               Text(
@@ -60,87 +64,100 @@ class FoodListState extends State<FoodList> {
           ),
         ),
       ));
-}
-
-PreferredSizeWidget _appBar(BuildContext context) {
-  return AppBar(
-    leading: IconButton(
-      icon: const FaIcon(FontAwesomeIcons.dice),
-      onPressed: () {},
-    ),
-    title: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Icon(Icons.location_on_outlined, color: LightThemeColor.accent),
-        Text(
-          "Location",
-          style: Theme.of(context).textTheme.bodyLarge,
+  PreferredSizeWidget _appBar(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: const FaIcon(FontAwesomeIcons.dice),
+        onPressed: () {},
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.location_on_outlined, color: LightThemeColor.accent),
+          Text(
+            "Location",
+            style: Theme.of(context).textTheme.bodyLarge,
+          )
+        ],
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Badge(
+            badgeStyle: const BadgeStyle(badgeColor: LightThemeColor.accent),
+            badgeContent: const Text(
+              "2",
+              style: TextStyle(color: Colors.white),
+            ),
+            position: BadgePosition.topStart(start: -3),
+            child: const Icon(Icons.notifications_none, size: 30),
+          ),
         )
       ],
-    ),
-    actions: [
-      IconButton(
-        onPressed: () {},
-        icon: Badge(
-          badgeStyle: const BadgeStyle(badgeColor: LightThemeColor.accent),
-          badgeContent: const Text(
-            "2",
-            style: TextStyle(color: Colors.white),
-          ),
-          position: BadgePosition.topStart(start: -3),
-          child: const Icon(Icons.notifications_none, size: 30),
-        ),
-      )
-    ],
-  );
-}
-Widget _searchBar() {
-  return const Padding(
-    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-    child: TextField(
-      decoration: InputDecoration(
-        hintText: 'Search food',
-        prefixIcon: Icon(Icons.search, color: Colors.grey),
-      ),
-    ),
-  );
-}
+    );
+  }
 
-Widget _categories() {
-  return Padding(
+  Widget _searchBar() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search food',
+          prefixIcon: Icon(Icons.search, color: Colors.grey),
+        ),
+      ),
+    );
+  }
+
+  Widget _categories() {
+    return Padding(
       padding: const EdgeInsets.only(top: 8.0),
       child: SizedBox(
         height: 40,
         child: ListView.separated(
             scrollDirection: Axis.horizontal,
             itemBuilder: (_, index) {
+              final category = AppData.categories[index];
               return GestureDetector(
                 onTap: () {
-                  print('Кликнули на категорию');
+                  onCategoryTap(index);
                 },
                 child: Container(
                   width: 100,
                   alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                    color: LightThemeColor.accent,
-                    borderRadius: BorderRadius.all(
+                  decoration: BoxDecoration(
+                    color: category.isSelected
+                        ? LightThemeColor.accent
+                        : Colors.transparent,
+                    borderRadius: const BorderRadius.all(
                       Radius.circular(15),
                     ),
                   ),
-                  child: const Text(
-                    'Kebab',
-                    // style: Theme.of(context).textTheme.headlineMedium,
+                  child: Text(
+                    category.type.name,
+                    style: Theme.of(context).textTheme.headlineMedium,
                   ),
                 ),
               );
             },
             separatorBuilder: (_, __) => Container(
                   width: 15,
-                  height: 30,
                 ),
-            itemCount: 10),
-      ));
+            itemCount: AppData.categories.length),
+      ),
+    );
+  }
+
+  void onCategoryTap(int selectedIndex) {
+    //Меняем выбранную категорию
+    AppData.categories.asMap().forEach((index, category) {
+      category.isSelected = index == selectedIndex;
+    });
+    setState(() {});
+  }
 }
+
+
 
 
 
