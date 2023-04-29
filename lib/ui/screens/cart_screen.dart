953573ad732e809b9a543e0bc73ea948a8ede75a
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-04-29 11:44:27 +0300

    appbar экрана корзины

--- lib/ui/screens/cart_screen.dart
+++ lib/ui/screens/cart_screen.dart
@@ -4,12 +4,23 @@ class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
 
   @override
-  State<CartScreen> createState() => CartScreenState();
+  State<CartScreen> createState() => _CartScreenState();
 }
 
-class CartScreenState extends State<CartScreen> {
+class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: _appBar(context),
+    );
   }
+}
+
+PreferredSizeWidget _appBar(BuildContext context) {
+  return AppBar(
+    title: Text(
+      "Card screen",
+      style: Theme.of(context).textTheme.displayMedium,
+    ),
+  );
 }
