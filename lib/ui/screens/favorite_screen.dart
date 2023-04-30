3a7111effe4daff562d8ace0499e968609eda2a9
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-04-30 19:13:10 +0300

    AppBar и EmptyWrapper экрана любимой еды

--- lib/ui/screens/favorite_screen.dart
+++ lib/ui/screens/favorite_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../widgets/empty_wrapper.dart';
+
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -10,6 +12,25 @@ class FavoriteScreen extends StatefulWidget {
 class FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: _appBar(context),
+      body: EmptyWrapper(
+        type: EmptyWrapperType.favorite,
+        title: 'Empty favorite',
+        isEmpty: true,
+        child: Container(),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _appBar(BuildContext context) {
+    return AppBar(
+      title: Text(
+        "Favorite screen",
+        style: Theme.of(context).textTheme.displayMedium,
+      ),
+    );
   }
 }
+
+
